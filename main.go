ebf57d05ff90d98fcbc5ac4a660e7d105fb3c3fe
harsh082ip <harshhvstech1975@gmail.com>
2024-08-30 04:28:59 +0530

    Initial Commit

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	router := gin.Default()
+
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"msg": "Welcome to sample Go Application :)",
+		})
+	})
+
+	router.GET("/data", func(ctx *gin.Context) {
+		ip := ctx.RemoteIP()
+		ctx.JSON(http.StatusOK, gin.H{
+			"status": 200,
+			"msg":    "Your Public IP is: " + ip,
+		})
+	})
+
+	router.GET("/health", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"msg": "Everything looks good here :)",
+		})
+	})
+
+	if err := router.Run(":8003"); err != nil {
+		log.Println("Error starting the server on port 8003 :(", err.Error())
+	}
+}
